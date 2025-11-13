9de091e9bed710fa1a7c55dd6ec09e6b8201c198
Sunjoo Lee <86330886+sunjoolee@users.noreply.github.com>
2025-11-13 20:31:31 +0900

    Update profile and blog settings in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,40 +1,32 @@
 const CONFIG = {
-  // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "sunjoolee",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "android / frontend developer",
+    bio: "focus on improving, not proving.",
+    email: "sheisthepoem@naver.com",
+    linkedin: "",
+    github: "sunjoolee",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `오늘여행`,
+      href: "https://play.google.com/store/apps/details?id=com.twoday.todaytrip&hl=ko",
     },
   ],
-  // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "sunjoolee-archaive",
+    description: "my knowledge base archaive.",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
-
-  // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://sunjoolee-archaive.vercel.app",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-
-  // notion configuration (required)
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID,
   },
-
-  // plugin configuration (optional)
   googleAnalytics: {
     enable: false,
     config: {
